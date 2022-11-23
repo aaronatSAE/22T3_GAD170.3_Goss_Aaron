9a64a72aa4eb0184d5a9cc9933551fe4e9f586c9
Aaron G <37372591+aaronatSAE@users.noreply.github.com>
2022-11-23 13:04:01 +1100

    Brambles updated

---
+++ Assets/Scripts/Detection.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if player has pressed E
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // DO THE THING
+            // 1. set UI gameobject to ACTIVE
+            // 2. if the player presses the button, active the TRANSIENT BRICKS
+        }
+    }
+}
---
+++ Assets/Scripts/SpikyBrambles.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AaronGoss
+{
+    public class SpikyBrambles : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            
+        }
+    }
+}
